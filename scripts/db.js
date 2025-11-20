af9fa778468eeda41ccb808021f27520cca35cdc
Ellen Santos <ellensantos10@hotmail.com>
2025-11-20 19:27:21 +0100

    JSON for main dishes created

---
+++ scripts/db.js
@@ -0,0 +1,32 @@
+let myDishes = [
+    {
+        "name": "Naruto Noodles",
+        "price": 12.99,
+        "description":" mit Gemüse und Toppings"
+},
+ {
+        "name": "Ramen Noodles mit Rindfleisch",
+        "price": 11.99,
+        "description":"mit Gemüse und Rindfleischstreifen"
+},
+ {
+        "name": "Pizza Margherita",
+        "price": 10.99,
+        "description":"mit Mozarella"
+},
+ {
+        "name": "Pizza Salami",
+        "price": 11.99,
+        "description":"mit Salamischeiben und Mozarella"
+},
+ {
+        "name": "Pizza Diavolo",
+        "price": 11.99,
+        "description":" mit Salami, Zwiebeln, Peperoni und Knoblauch"
+},
+ {
+        "name": "Pizza Garnelen",
+        "price": 11.99,
+        "description":"mit Zwiebeln und Garnelen"
+}
+]
