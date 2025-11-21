a7dafda190b176808b4e194e52df6d6aca5bf30f
Ellen Santos <ellensantos10@hotmail.com>
2025-11-21 18:22:44 +0100

    templates updated

--- scripts/template.js
+++ scripts/template.js
@@ -1,21 +1,25 @@
 function dishesTemplate(i) {
     return `<div class="borderlineDiv">
-        <h3>${myDishes[i].name}</h3>
-        <p>${myDishes[i].description}</p>
-        <p class="orangeBold">${myDishes[i].price}</p>
+         <div class="movePlusDiv">
+          <h3>${myDishes[i].name}</h3>
+          <p>${myDishes[i].description}</p>
+          <p class="orangeBold">${myDishes[i].price}</p>
+          <div class="plusDiv"><img class="plusIMG" src="./assets/Icons/icons8-plus-24.png" alt="Zum Warenkorb Hinzufügen Zeichen">
+         </div>
+         </div>
         </div>
-    <div class="plusDiv">
-            </div>
             `;
 }
 function sideDishesTemplate(i) {
     return `<div class="borderlineDiv">
-        <h3>${mySideDishes[i].name}</h3>
-        <p>${mySideDishes[i].description}</p>
-        <p class="orangeBold">${mySideDishes[i].price}</p>
+        <div class="movePlusDiv">
+          <h3>${mySideDishes[i].name}</h3>
+           <p>${mySideDishes[i].description}</p>
+           <p class="orangeBold">${mySideDishes[i].price}</p>
+        <div class="plusDiv"><img class="plusIMG" src="./assets/Icons/icons8-plus-24.png" alt="Zum Warenkorb Hinzufügen Zeichen">
+         </div>
+         </div>
         </div>
-    <div class="plusDiv">
-            </div>
             `;
 
 }
