73302a592568e374e9b6a82cae3fd2d2da08d3f6
Ellen Santos <ellensantos10@hotmail.com>
2025-11-23 19:25:05 +0100

    basket style changed

--- scripts/template.js
+++ scripts/template.js
@@ -25,28 +25,29 @@ function sideDishesTemplate(i) {
 }
 function addtoBasketDishes(i) {
     const item = basket[i];
-    return `<h3>${item.name}</h3>
+    return `<h3 style="padding-left: 10px;" >${item.name}</h3>
                     <div class="bskContent" >
                         <img class="bskIcons" onclick="changeAmount(${i}, -1)" src="./assets/Icons/icons8-minus-24.png" alt="Minus Icon">
                         <span>${item.amount}x</span>
                         <img class="bskIcons" onclick="changeAmount(${i}, 1)" src="./assets/Icons/icons8-plus-24.png" alt="Plus Icon">
                         <span>${(item.amount * item.price).toFixed(2)} €</span>
                         <img class="bskIcons" onclick="deleteItem(${i})" src="./assets/Icons/icons8-müll-48.png" alt="Müll Icon">
-                    </div>`
+                    </div>
+                    <hr>`
 }
 function basketPrice(total) {
     return ` <table>
                         <tr>
                             <td> Zwischensumme </td>
-                            <td style="padding-left: 120px;"> ${total.toFixed(2)} € </td>
+                            <td style="padding-left: 116px;"> ${total.toFixed(2)} € </td>
                         </tr>
                         <tr>
                             <td> Lieferkosten </td>
-                            <td style="padding-left: 127px;"> 5.00 € </td>
+                            <td style="padding-left: 124px;"> 5.00 € </td>
                         </tr>
                         <tr>
                             <td> Gesamt </td>
-                            <td style="padding-left: 120px;"> ${(total + 5).toFixed(2)} € </td>
+                            <td style="padding-left: 116px;"> ${(total + 5).toFixed(2)} € </td>
                         </tr>
                     </table>`
 }
